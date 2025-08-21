183a95277fd4057e44532365b68bead89983a2e7
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2025-08-21 11:58:03 +0100

    add monitoring layer

--- terraform/output.tf
+++ terraform/output.tf
@@ -31,3 +31,36 @@ output "user_assigned_identity_client_id" {
 output "user_assigned_identity_principal_id" {
   value = azurerm_user_assigned_identity.keyvault_identity.principal_id
 }
+
+# Required outputs for GitHub Actions pipeline
+output "cluster_name" {
+  description = "The name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.main.name
+  # Note: Replace 'main' with your actual resource group resource name
+}
+
+output "azure_client_id" {
+  description = "The client ID of the user assigned identity for workload identity"
+  value       = azurerm_user_assigned_identity.keyvault_identity.client_id
+}
+
+output "azure_tenant_id" {
+  description = "The Azure tenant ID"
+  value       = data.azurerm_client_config.current.tenant_id
+}
+
+# Additional useful outputs
+output "subscription_id" {
+  description = "The Azure subscription ID"
+  value       = data.azurerm_client_config.current.subscription_id
+}
+
+output "keyvault_uri" {
+  description = "The URI of the Key Vault"
+  value       = azurerm_key_vault.aks_keyvault.vault_uri
+}
